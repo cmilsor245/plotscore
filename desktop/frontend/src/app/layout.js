5557383539745ca77d7c32cd5ce60d13abd3652b
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-12 19:47:39 +0100

    adding vercel analytics

--- desktop/frontend/src/app/layout.js
+++ desktop/frontend/src/app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
+
 export const metadata = {
   title: 'plotscore',
   description: 'The plotscore project consists of a website based on reviews/ratings of all kinds of mainstream multimedia content, meaning content that can be consumed on major streaming platforms or in theaters or similar venues.',
