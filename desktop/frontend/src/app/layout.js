cf3018448065aea56a13dc10691415cb2d66b1b6
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-14 18:04:49 +0100

    finish frontend landing page ahead of starting backend development

--- desktop/frontend/src/app/layout.js
+++ desktop/frontend/src/app/layout.js
@@ -3,7 +3,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata = {
   title: 'plotscore',
-  description: 'The plotscore project consists of a website based on reviews/ratings of all kinds of mainstream multimedia content, meaning content that can be consumed on major streaming platforms or in theaters or similar venues.',
+  description: 'The plotscore project consists of a website based on reviews/ratings of all kinds of mainstream multimedia content, meaning content that can be consumed on major streaming platforms or in theaters or similar venues.'
 }
 
 export default function RootLayout({ children }) {
--- desktop/frontend/src/app/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1>not found</h1>
-      <p>you just hit a route that doesn't exist... the sadness.</p>
-    </div>
-  )
-}
