bbcc18aef9e0879939a063c59fac8cd18d07dff9
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-19 20:03:58 +0100

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'npm',
+      args: 'start',
+      cwd: './desktop/frontend',
+      interpreter: 'none',
+    },
+    {
+      name: 'laravel-app',
+      script: 'php',
+      args: 'artisan serve',
+      cwd: './desktop/backend',
+    },
+  ],
+}
