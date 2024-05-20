054dc7b5cdc39c8a7528a989f1473d28e2b5bcaf
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-20 01:32:48 +0100

    removing vercel analytics components

--- desktop/frontend/src/app/layout.js
+++ desktop/frontend/src/app/layout.js
@@ -1,6 +1,3 @@
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-
 export const metadata = {
   title: 'plotscore',
   description: 'The plotscore project consists of a website based on reviews/ratings of all kinds of mainstream multimedia content, meaning content that can be consumed on major streaming platforms or in theaters or similar venues.'
@@ -11,8 +8,6 @@ export default function RootLayout({ children }) {
     <html lang = 'en'>
       <body>
         { children }
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
