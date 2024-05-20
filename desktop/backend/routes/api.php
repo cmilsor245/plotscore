f00037cfdf62326eb80f79012513f13b9ade9fcf
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-20 01:18:54 +0100

    testing that code is being cloned correctly

--- desktop/backend/routes/api.php
+++ desktop/backend/routes/api.php
@@ -10,3 +10,5 @@ Route::middleware('auth:sanctum') -> group(function () {
   Route::get('user', [AuthController::class, 'user']);
   Route::post('logout', [AuthController::class, 'logout']);
 });
+
+// test comment now
