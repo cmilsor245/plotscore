a9b3040055cbaf883f49bfc16a06a16766281b8f
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-22 20:57:14 +0100

    trying to fix favicon not found status

--- desktop/frontend/src/app/layout.js
+++ desktop/frontend/src/app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang = 'en'>
+      <head>
+        <link rel = 'icon' href = '/favicon.ico' />
+      </head>
       <body>
         { children }
       </body>
