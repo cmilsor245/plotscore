50ee874ce2363510fc9f995c332380ba93a0cbf8
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-22 19:12:04 +0100

    trying to add the default admin manually

--- desktop/backend/database/seeders/AdminSeeder.php
+++ desktop/backend/database/seeders/AdminSeeder.php
@@ -4,23 +4,39 @@ namespace Database\Seeders;
 use App\Http\Controllers\AuthController;
 use App\Http\Requests\AuthRequest;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder {
   public function run(): void {
-    $authController = new AuthController();
+    // $authController = new AuthController();
 
-    $request = AuthRequest::create('createAdmin', 'POST', [
-      'secret_key' => 'plotscore_christian_admin_secret_key',
+    // $request = AuthRequest::create('createAdmin', 'POST', [
+    //   'secret_key' => 'plotscore_christian_admin_secret_key',
 
+    //   'username' => 'plotscore',
+
+    //   'email' => 'hello@plotscore.com',
+    //   'password' => 'password1234',
+
+    //   'bio' => 'still not over dune: part two',
+
+    //   'location' => 'la la land',
+    //   'website' => 'plotscore--desktop.vercel.app'
+    // ]);
+
+    // $authController -> createAdmin($request);
+
+    /* ----------------------- */
+
+    DB::table('users') -> insert([
+      'role' => 'admin',
       'username' => 'plotscore',
-
       'email' => 'hello@plotscore.com',
-      'password' => 'password1234',
-
-      'location' => 'hollywood',
-      'website' => 'plotscore-desktop.vercel.app'
+      'password' => Hash::make('password1234'),
+      'location' => 'la la land',
+      'website' => 'plotscore--desktop.vercel.app',
+      'pronouns' => 'they/them'
     ]);
-
-    $authController -> createAdmin($request);
   }
 }
