b669dfdfff0da05cd9a33130e3421455bf863f27
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-22 18:50:46 +0100

    trying allowing everyone to add admin users

--- desktop/backend/routes/api.php
+++ desktop/backend/routes/api.php
@@ -2,11 +2,11 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('create-admin', [AuthController::class, 'createAdmin']);
 Route::post('signup', [AuthController::class, 'signup']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum') -> group(function () {
-  Route::post('create-admin', [AuthController::class, 'createAdmin']);
   Route::get('user', [AuthController::class, 'user']);
   Route::post('logout', [AuthController::class, 'logout']);
 });
