ba9761fb777fd7dd3e684217d9b392b5eba34423
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-25 00:15:43 +0100

    minor changes

--- desktop/frontend/components/common/side-menu.js
+++ desktop/frontend/components/common/side-menu.js
@@ -31,8 +31,7 @@ export default function SideMenu({
 }) {
   const publicApiStoragePath = process.env.NEXT_PUBLIC_API_STORAGE_PATH
 
-  // const avatarSrc = `${ publicApiStoragePath }${ userData ? userData.avatar : '/storage/avatars/default.png' }`
-  const avatarSrc = 'https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b'
+  const avatarSrc = `${ publicApiStoragePath }${ userData ? userData.avatar : '/storage/avatars/default.png' }`
 
   /* ------------------------------- */
 
