5783073f2bcf43e855f1e07a66f79a075d5a4b6b
christianms13 <christiaanmillaan1313@gmail.com>
2024-05-30 21:46:32 +0100

    finishing landing page for mobile version

--- mobile/scripts/translation--landing-page.js
+++ mobile/scripts/translation--landing-page.js
@@ -3,6 +3,9 @@ const translationsEN = {
     ACCESS_BUTTONS: {
       SIGN_UP: 'get started',
       LOG_IN: 'log in'
+    },
+    FOOTER: {
+      COPYRIGHT: '© 2024 - Made by Christian Millán Soria'
     }
   }
 }
@@ -12,6 +15,9 @@ const translationsES = {
     ACCESS_BUTTONS: {
       SIGN_UP: 'registrarse',
       LOG_IN: 'iniciar sesión'
+    },
+    FOOTER: {
+      COPYRIGHT: '© 2024 - Hecho por Christian Millán Soria'
     }
   }
 }
@@ -30,7 +36,8 @@ function translate(lang, namespace, section, key) {
 function translatePage(lang) {
   const elementsToTranslate = [
     { selector: '.signup-button', namespace: 'LANDING_PAGE', section: 'ACCESS_BUTTONS', key: 'SIGN_UP' },
-    { selector: '.login-button', namespace: 'LANDING_PAGE', section: 'ACCESS_BUTTONS', key: 'LOG_IN' }
+    { selector: '.login-button', namespace: 'LANDING_PAGE', section: 'ACCESS_BUTTONS', key: 'LOG_IN' },
+    { selector: '.copyright', namespace: 'LANDING_PAGE', section: 'FOOTER', key: 'COPYRIGHT' }
   ]
 
   elementsToTranslate.forEach(item => {
