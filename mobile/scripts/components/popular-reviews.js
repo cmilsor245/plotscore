b9b8eee6ebc9d45a6ab5e8851247c3ac2713f8af
christianms13 <christiaanmillaan1313@gmail.com>
2024-06-02 15:56:06 +0100

    finishing home page with full dynamic info rendering

--- mobile/scripts/components/popular-reviews.js
+++ mobile/scripts/components/popular-reviews.js
@@ -23,6 +23,16 @@ document.addEventListener("DOMContentLoaded", async () => {
       poster.dataset.highResSrc = item.highResPosterSrc
       mainInfoSection.appendChild(poster)
 
+      if (item.hasEpisodeNumber) {
+        const divEpisode = document.createElement('div')
+        divEpisode.classList.add('episode')
+        const pEpisode = document.createElement('p')
+        const episodeNumber = item.episodeNumber < 10? `e0${ item.episodeNumber }` : `e${ item.episodeNumber }`
+        pEpisode.textContent = episodeNumber.toUpperCase()
+        divEpisode.appendChild(pEpisode)
+        poster.appendChild(divEpisode)
+      }
+
       /* --------------------------------------------------- */
 
       const mainInfoDeatils = document.createElement('section')
@@ -51,13 +61,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       const mediaTitle = document.createElement('h5')
       mediaTitle.classList.add('main-info__media-title')
-      mediaTitle.textContent = item.title
+      mediaTitle.innerHTML = item.title
       mediaDetails.appendChild(mediaTitle)
-
-      const mediaYear = document.createElement('h6')
-      mediaYear.classList.add('main-info__media-year')
-      mediaYear.textContent = item.year
-      mediaDetails.appendChild(mediaYear)
 
       mainInfoDeatils.appendChild(mediaDetails)
 
@@ -95,12 +100,14 @@ document.addEventListener("DOMContentLoaded", async () => {
       commentIcon.classList.add('ti', 'ti-message')
       commentCount.appendChild(commentIcon)
 
-      const commentCountText = document.createElement('p')
-      commentCountText.classList.add('main-info__comment-count-number')
-      commentCountText.textContent = item.commentCount
-      commentCount.appendChild(commentCountText)
+      if (item.commentCount > 0) {
+        const commentCountText = document.createElement('p')
+        commentCountText.classList.add('main-info__comment-count-number')
+        commentCountText.textContent = item.commentCount
+        commentCount.appendChild(commentCountText)
 
-      ratingAndCommentCount.appendChild(commentCount)
+        ratingAndCommentCount.appendChild(commentCount)
+      }
 
       mainInfoDeatils.appendChild(ratingAndCommentCount)
 
@@ -157,7 +164,15 @@ document.addEventListener("DOMContentLoaded", async () => {
       return review
     })
 
-    articles.forEach(article => container.appendChild(article))
+    articles.forEach((article, index) => {
+      container.appendChild(article)
+
+      if (index < articles.length - 1) {
+        const divider = document.createElement('div')
+        divider.classList.add('divider')
+        container.appendChild(divider)
+      }
+    })
 
     window.addEventListener('load', () => {
       articles.forEach(article => {
