151132404d6fa7cf322e53c7322ef98a75c3445e
christianms13 <christiaanmillaan1313@gmail.com>
2024-06-04 00:10:49 +0100

    testing admin creation process secret key checking addition

--- desktop/backend/database/seeders/AdminSeeder.php
+++ desktop/backend/database/seeders/AdminSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 use App\Http\Controllers\AuthController;
 use App\Http\Requests\AuthRequest;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder {
   public function run(): void {
@@ -26,23 +24,5 @@ class AdminSeeder extends Seeder {
     ]);
 
     $authController -> createAdmin($request);
-
-    /* ----------------------- */
-
-    // DB::table('users') -> insert([
-    //   'role' => 'admin',
-
-    //   'username' => 'plotscore',
-
-    //   'email' => 'hello@plotscore.com',
-    //   'password' => Hash::make('password1234'),
-
-    //   'avatar' => '/storage/avatars/default.png',
-    //   'bio' => 'still not over dune: part two',
-
-    //   'location' => 'la la land',
-    //   'website' => 'plotscore--desktop.vercel.app',
-    //   'pronouns' => 'they/them'
-    // ]);
   }
 }
